0cb6184d3affa33649b54c1818a83fd2d628ed08
Juan Manuel Martin <juanna@ludiverso.com>
2014-03-24 20:06:47 +0100

    Correcciones según comentarios Gabriel

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,6 @@ class Order < ActiveRecord::Base
   	has_many :line_items, :dependent => :destroy
   	has_many :products, through: :line_items
 
-	accepts_nested_attributes_for :line_items, :allow_destroy => true
+	accepts_nested_attributes_for :line_items, reject_if: proc {|attributes| attributes['price'].blank?} , :allow_destroy => true
 	
 end
