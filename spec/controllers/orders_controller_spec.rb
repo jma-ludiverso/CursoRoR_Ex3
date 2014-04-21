d22e6ad05458234944c7b44ba07420bbc5691df6
Juan Manuel Martin <juanna@ludiverso.com>
2014-04-21 22:58:16 +0200

    Ejercicio semana 8 - Correcciones profesor

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 describe OrdersController do
-	integrate_views 
 
 	it "require login" do
-		get :new
-		expect(response).to be_success
-		expect(response.status).to eq(200)	  		
+		get :new, locale: 'es'
+		expect(response).to redirect_to(root_url)	
 	end
 
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,7 +4,8 @@ describe SessionsController do
 
 	describe "#create" do
 	    it "redirect to orders path after login" do
-			post :create, post: { :email => 'juanma@ludiverso.com', :password => 'pepe'}
+	    	user = User.create!(name: "user test", email: "test@test.com", password: "passtest123", password_confirmation: "passtest123")
+			post :create, locale: 'es', email: user.email, password: user.password
 	      	expect(response).to redirect_to(orders_path)
 	    end
 	end
